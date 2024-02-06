73c1145f91f30b3ff6f65ff3d9d309c978bbf993
Tejas A. Sheth <tejas1004sheth@gmail.com>
2024-02-06 14:08:09 -0800

    Added Scripts - v1
    Date. 06 Feb
    - Implemented horizontal player movement + camera movement

---
+++ CSCI526/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
+    private float inputAxis;
+    private Vector2 velocity;
+    public float moveSpeed = 5f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement()
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x + 0.5f);
+        
+        rigidbody.MovePosition(position);
+    }
+}
